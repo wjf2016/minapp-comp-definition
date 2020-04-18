b1fcb468cec1b03938f360ee7c4c6123913d0423
wujianfu <wujianfu@camel.com.cn>
2020-04-18 15:29:16 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,21 @@
+# minapp-comp-definition 小程序组件提示器
+
+## 功能
+
+开发原生小程序时，可以在 wxml 页面，'alt + 点击自定义组件的标签名'跳转到相对应的组件页面
+
+支持页面 json 配置和 app.json 全局配置的组件声明。
+
+组件路径支持绝对路径和相对路径。
+
+** 注意 **
+
+小程序元素组件将不会显示定义提示，目前有以下原生组件名：
+
+'movable-view', 'cover-image', 'cover-view', 'movable-area', 'scroll-view', 'swiper', 'swiper-item', 'view', 'icon', 'progress', 'rich-text', 'text', 'button', 'checkbox', 'checkbox-group', 'editor', 'form', 'input', 'label', 'picker', 'picker-view', 'picker-view-column', 'radio', 'radio-group', 'slider', 'switch', 'textarea', 'functional-page-navigator', 'navigator', 'audio', 'camera', 'image', 'live-player', 'live-pusher', 'video', 'map', 'canvas', 'ad', 'official-account', 'open-data', 'web-view'
+
+## Github
+
+有任何问题都欢迎提出，或者有需求也可以提出！
+
+[https://github.com/wjf2016/minapp-comp-definition](https://github.com/wjf2016/minapp-comp-definition)
---
+++ src/extension.ts
@@ -0,0 +1,127 @@
+import { log } from 'util';
+import * as vscode from 'vscode';
+const fs = require('fs');
+const path = require('path');
+const wxTags = [
+  'movable-view',
+  'cover-image',
+  'cover-view',
+  'movable-area',
+  'scroll-view',
+  'swiper',
+  'swiper-item',
+  'view',
+  'icon',
+  'progress',
+  'rich-text',
+  'text',
+  'button',
+  'checkbox',
+  'checkbox-group',
+  'editor',
+  'form',
+  'input',
+  'label',
+  'picker',
+  'picker-view',
+  'picker-view-column',
+  'radio',
+  'radio-group',
+  'slider',
+  'switch',
+  'textarea',
+  'functional-page-navigator',
+  'navigator',
+  'audio',
+  'camera',
+  'image',
+  'live-player',
+  'live-pusher',
+  'video',
+  'map',
+  'canvas',
+  'ad',
+  'official-account',
+  'open-data',
+  'web-view',
+];
+const appFile = 'app.json';
+let rootPath = '';
+
+function lastLevelDir(filePath: string): string {
+  return path.dirname(filePath);
+}
+
+function findRootPath(path: string): string {
+  const dir = lastLevelDir(path);
+  const files = fs.readdirSync(dir);
+
+  if (files.includes(appFile)) {
+    return dir;
+  } else {
+    return findRootPath(dir);
+  }
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.languages.registerDefinitionProvider(
+      [{ scheme: 'file', language: 'wxml', pattern: '**/*.wxml' }],
+      {
+        provideDefinition(
+          doc: vscode.TextDocument,
+          position: vscode.Position,
+          token: vscode.CancellationToken,
+        ) {
+          const lineText = doc.lineAt(position).text;
+          const wordRange = doc.getWordRangeAtPosition(position, /[\w|\-]+\b/);
+          const tag = (lineText.match(/(?<=<\/?)[\w|\-]+\b/) || [])[0];
+          const word = doc.getText(wordRange);
+
+          if (!tag) {
+            return;
+          }
+
+          if (tag !== word) {
+            return;
+          }
+
+          if (wxTags.includes(tag)) {
+            return [];
+          }
+
+          const filePath = doc.fileName;
+          let jsonFile = filePath.replace('.wxml', '.json');
+
+          if (!rootPath) {
+            rootPath = findRootPath(filePath);
+          }
+
+          let config = JSON.parse(fs.readFileSync(jsonFile).toString());
+          let compPath;
+
+          if (config.usingComponents && config.usingComponents[tag]) {
+            compPath = config.usingComponents[tag];
+          }
+
+          // 页面或者组件没有定义，查找一下全局配置
+          if (!compPath) {
+            jsonFile = path.join(rootPath, appFile);
+            config = JSON.parse(fs.readFileSync(jsonFile).toString());
+
+            if (config.usingComponents && config.usingComponents[tag]) {
+              compPath = config.usingComponents[tag];
+            }
+          }
+
+          const componentPath = path.join(rootPath, `${compPath}.js`);
+
+          return new vscode.Location(
+            vscode.Uri.file(componentPath),
+            new vscode.Position(0, 0),
+          );
+        },
+      },
+    ),
+  );
+}
